8a74098dd72fe85123925dde1e0b9b1b278a5e42
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2014-12-04 08:19:13 +0100

    use convention when naming main arguments

--- ray.c
+++ ray.c
@@ -2,17 +2,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(/*int argc, char *argv[]*/ L, c, b) char**c, **b; {
+int main(int argc, char *argv[]) {
+  char **c;
   for (int y = -22; y < 22; y += 2) {
     for (int x = -39; ++x < 37; ) {
-      b = c + 1;
+      c = argv + 1;
       float h = 0;
       float i = 9;
-      while (*b) {
-        int sphereX = atoi(*b++);
-        int sphereY = atoi(*b++);
-        int sphereZ = 50 - atoi(*b++);
-        int sphereR = atoi(*b++);
+      while (*c) {
+        int sphereX = atoi(*c++);
+        int sphereY = atoi(*c++);
+        int sphereZ = 50 - atoi(*c++);
+        int sphereR = atoi(*c++);
         
         int A = 2500 + x * x + y * y;
         int B = sphereZ * 50 - sphereY * y - sphereX * x;
