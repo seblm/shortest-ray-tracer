d9c82dc50f279a22adad1c836df67ed009e8fdfe
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2014-12-04 08:56:21 +0100

    make it more readable

--- ray.c
+++ ray.c
@@ -1,2 +1,31 @@
-#define s atoi(*b++)
-float A,B,i,o,w=-22,_,h,r,d,e,f;main(L,c,b)char**c,**b;{for(;w<22;w+=2){for(o=-39;++o<37;){b=c+1,h=0,i=9;while(*b)d=s,e=s,f=50-s,r=s,A=2500+o*o+w*w,B=f*50-e*w-d*o,(_=B*B-A*(f*f+d*d+e*e-r*r))>0&&(_=(B-sqrt(_))/A,_<i&&(h=(f-_*(50+o+w)-d-e)/r,i=_));putchar("......-:=#@"[(int)(h*3)+5]);}puts("");}}
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+float A, B, i, _, h;
+
+int main(/*int argc, char *argv[]*/ L, c, b) char**c, **b; {
+  for (int y = -22; y < 22; y += 2) {
+    for (int x = -39; ++x < 37; ) {
+      b = c + 1;
+      h = 0;
+      i = 9;
+      while (*b) {
+        int sphereX = atoi(*b++);
+        int sphereY = atoi(*b++);
+        int sphereZ = 50 - atoi(*b++);
+        int sphereR = atoi(*b++);
+        
+        A = 2500 + x * x + y * y;
+        B = sphereZ * 50 - sphereY * y - sphereX * x;
+        
+        (_ = B * B - A * (sphereZ * sphereZ + sphereX * sphereX + sphereY * sphereY - sphereR * sphereR)) > 0
+          && (_ = (B - sqrt(_)) / A, _ < i
+            && (h = (sphereZ - _ * (50 + x + y) - sphereX - sphereY) / sphereR, i = _));
+      }
+      putchar("......-:=#@"[(int)(h * 3) + 5]);
+    }
+    puts("");
+  }
+  return 0;
+}
